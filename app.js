09a5f2add4c337e4382d8e480bcea9cd6f340bff
Dru Serkes <DruSerkes@gmail.com>
2020-08-16 23:45:25 -0400

    add secrets

--- app.js
+++ app.js
@@ -1,42 +1,37 @@
 const $form = $('#searchform');
-const api_key = 'cUHWqT99EuUFVOfIEpNJHdqC4dj0Si2q';
 const $searchInput = $('#searchInput');
 const $gifContainer = $('#gif-container');
 
-// creates  gif and appends it to the DOM 
+// creates  gif and appends it to the DOM
 const makeGif = (response) => {
-    let randIdx = Math.floor(Math.random() * response.data.length) 
-    const gifUrl = response.data[randIdx].images.original.url;
+	let randIdx = Math.floor(Math.random() * response.data.length);
+	const gifUrl = response.data[randIdx].images.original.url;
 
+	let $newCol = $('<div>', { class: 'col-12 col-md-4 mb-1' });
+	let $newImg = $('<img>', { src: gifUrl });
 
-    let $newCol = $('<div>', { 'class': 'col-12 col-md-4 mb-1'});
-    let $newImg = $('<img>', { 'src': gifUrl });
+	$newCol.append($newImg);
+	$gifContainer.append($newCol);
+};
 
-    $newCol.append($newImg);
-    $gifContainer.append($newCol);
-}
+//listens for search, adds new img to DOM
+$form.on('submit', async function(e) {
+	e.preventDefault();
 
-//listens for search, adds new img to DOM 
-$form.on('submit', async function(e){
-    e.preventDefault();
+	//get search input
+	let term = $searchInput.val();
+	$searchInput.val('');
 
-    //get search input
-    let term = $searchInput.val();
-    $searchInput.val('');
+	//request response from giphy
+	const gifParams = { params: { api_key, q: term } };
+	const res = await axios.get('https://api.giphy.com/v1/gifs/search', gifParams);
 
-    //request response from giphy
-    const gifParams = { params: { api_key, q: term } };
-    const res = await axios.get('http://api.giphy.com/v1/gifs/search', gifParams);
-    
-    //do something with the data
-    makeGif(res.data);
+	//do something with the data
+	makeGif(res.data);
 });
 
-$('#remove').on('click', e => {
-    $gifContainer.empty();
-})
+$('#remove').on('click', (e) => {
+	$gifContainer.empty();
+});
 
 
-
-// Giphy API Key: 
-// cUHWqT99EuUFVOfIEpNJHdqC4dj0Si2q
